db03115efd2d6509054f48ca1a66ead512370eff
Alvin Coulson <aaacoulson@gmail.com>
2020-03-20 13:46:37 +0000

    add scrapped NC county data

--- lib/nc_charter_schools/cli.rb
+++ lib/nc_charter_schools/cli.rb
@@ -1,7 +1,7 @@
 class NcCharterSchools::CLI
   def call
     puts "Welcome to NC Charter Schools CLI App!"
-    puts NcCharterSchools::Scraper.scrape_grade
+    puts NcCharterSchools::Scraper.scrape_nc_county
   end
   
 end
--- lib/nc_charter_schools/scraper.rb
+++ lib/nc_charter_schools/scraper.rb
@@ -10,31 +10,39 @@ class NcCharterSchools::Scraper
   end
   
   def self.scrape_url
-        @@url ||= doc.css("td.t15Body a").map {|url| url.attribute("href").value}
-    end
+    @@url ||= doc.css("td.t15Body a").map {|url| url.attribute("href").value}
+  end
 
-    def self.scrape_charter_code
-        @@charter_code ||= doc.css("td.apex_report_break").map {|code| code.children.text.scan(/Charter Code:\s\S+/).join.gsub("Charter Code: ", "")}
-    end
+  def self.scrape_charter_code
+    @@charter_code ||= doc.css("td.apex_report_break").map {|code| code.children.text.scan(/Charter Code:\s\S+/).join.gsub("Charter Code: ", "")}
+  end
 
-    def self.scrape_city_state
-        @@city_state ||= doc.css("td.apex_report_break").map {|cs| cs.children.text.scan(/\w+\s\w+\s\,\s\NC\s\d+|\w+\,\s\NC\s\d+|\w+\s\w+\,\s\NC\s\d+|\w+\-\w+\,\s\NC\s\d+/).join.gsub(/\D+\n/, "").gsub("\n", "")}.map {|e| e.gsub(/\A\d+/, "")}
-    end
+  def self.scrape_city_state
+    @@city_state ||= doc.css("td.apex_report_break").map {|cs| cs.children.text.scan(/\w+\s\w+\s\,\s\NC\s\d+|\w+\,\s\NC\s\d+|\w+\s\w+\,\s\NC\s\d+|\w+\-\w+\,\s\NC\s\d+/).join.gsub(/\D+\n/, "").gsub("\n", "")}.map {|e| e.gsub(/\A\d+/, "")}
+  end
 
-    def self.scrape_county
-        @@county ||= doc.css("td.apex_report_break").map {|county| county.children.text.scan(/County:\s\w+\s\w+/).join.gsub(/\nSchool/, "").gsub("County: ", "")}
-    end
+  def self.scrape_county
+    @@county ||= doc.css("td.apex_report_break").map {|county| county.children.text.scan(/County:\s\w+\s\w+/).join.gsub(/\nSchool/, "").gsub("County: ", "")}
+  end
 
-    def self.scrape_telephone
-        @@telephone ||= doc.css("td.apex_report_break").map {|tel| tel.children.text.scan(/Phone:\s\d+\.\d+\d.\d+/).join.gsub("Phone: ", "")}
-    end
+  def self.scrape_telephone
+    @@telephone ||= doc.css("td.apex_report_break").map {|tel| tel.children.text.scan(/Phone:\s\d+\.\d+\d.\d+/).join.gsub("Phone: ", "")}
+  end
 
-    def self.scrape_effective_date
-        @@effective_date ||= doc.css("td.apex_report_break").map {|date| date.children.text.scan(/School Effective Date:\s\d+\/\d+\/\d+/).join.gsub("School Effective Date: ","").gsub("/", "-")}
-    end
+  def self.scrape_effective_date
+    @@effective_date ||= doc.css("td.apex_report_break").map {|date| date.children.text.scan(/School Effective Date:\s\d+\/\d+\/\d+/).join.gsub("School Effective Date: ","").gsub("/", "-")}
+  end
 
-    def self.scrape_grade
-        @@grade ||= doc.css("tr.highlight-row").map {|grade| grade.children[3].text}
-    end
+  def self.scrape_grade
+    @@grade ||= doc.css("tr.highlight-row").map {|grade| grade.children[3].text}
+  end
+    
+  # Scraping data on NC counties from website of North Carolina Association of County Commissioners
+  def self.county_doc
+    @@county_doc ||= Nokogiri::HTML(open("https://www.ncacc.org/171/Links-to-Counties"))
+  end
 
+  def self.scrape_nc_county
+    @@nc_counties ||= county_doc.css("table.telerik-reTable-2 h2.subhead1").map {|c| c.text}.map {|e| e.gsub(/\u00A0|\n/, "")}.map {|e| e.gsub(/[^"]\s[^a-zA-z]/, "")}.map {|e| e.gsub(" County", "")}
+  end
 end
