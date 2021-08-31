303f5166b273c8afd5bbb98d89130eb5a1d468ee
Alvin Coulson <aaacoulson@gmail.com>
2021-08-31 14:34:19 -0400

    replace variable names on scraper.rb

--- lib/nc_charter_schools/scraper.rb
+++ lib/nc_charter_schools/scraper.rb
@@ -10,8 +10,8 @@ class NcCharterSchools::Scraper
   end
 
   def self.scrape_name
-    @@name ||= data_load.map do |name|
-      name.children.text.lstrip.scan(/[a-zA-Z\W\s]+(?=Charter Code)/)
+    @@name ||= data_load.map do |data|
+      data.children.text.lstrip.scan(/[a-zA-Z\W\s]+(?=Charter Code)/)
     end.flatten
   end
   
@@ -24,12 +24,12 @@ class NcCharterSchools::Scraper
   end
 
   def self.scrape_charter_code
-    @@charter_code ||= data_load.map {|code| code.children.text.scan(/(?<=Code:.)\S{3}/)}.flatten
+    @@charter_code ||= data_load.map {|data| data.children.text.scan(/(?<=Code:.)\S{3}/)}.flatten
   end
 
   def self.scrape_city_state
-    @@city_state ||= data_load.map do |cs|
-      cs.children.text.scan(/(?<=\n)[a-zA-Z\s\W]+\s\NC\s\d+/)
+    @@city_state ||= data_load.map do |data|
+      data.children.text.scan(/(?<=\n)[a-zA-Z\s\W]+\s\NC\s\d+/)
     end.flatten
   end
 
@@ -40,15 +40,15 @@ class NcCharterSchools::Scraper
   end
 
   def self.scrape_telephone
-    @@telephone ||= data_load.map {|tel| tel.children.text.scan(/Phone:\s\d+\.\d+\d.\d+/).join.gsub("Phone: ", "")}
+    @@telephone ||= data_load.map {|data| data.children.text.scan(/Phone:\s\d+\.\d+\d.\d+/).join.gsub("Phone: ", "")}
   end
 
   def self.scrape_effective_date
-    @@effective_date ||= data_load.map {|date| date.children.text.scan(/(?<=Date:.)\S{10}/)}.flatten.map {|date| date.gsub("/", "-")}
+    @@effective_date ||= data_load.map {|data| data.children.text.scan(/(?<=Date:.)\S{10}/)}.flatten.map {|date| date.gsub("/", "-")}
   end
 
   def self.scrape_grade
-    @@grade ||= doc.css("tr.highlight-row").map {|grade| grade.children[3].text}
+    @@grade ||= doc.css("tr.highlight-row").map {|data| data.children[3].text}
   end
     
   # Scraping data on NC counties from website of North Carolina Association of County Commissioners
@@ -57,8 +57,8 @@ class NcCharterSchools::Scraper
   end
 
   def self.scrape_nc_county
-    @@nc_counties ||= county_doc.css("div.map-select option").select do |v|
-      v.attributes.size == 3
-    end.map { |c| c.children.text }
+    @@nc_counties ||= county_doc.css("div.map-select option").select do |data|
+      data.attributes.size == 3
+    end.map { |data| data.children.text }
   end
 end
